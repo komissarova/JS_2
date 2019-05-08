bc7a17b5da5e55e315d3db0e90b58c69cc113004
komissarova <elya_komissarova@mail.ru>
2019-05-08 09:57:26 +0300

    Добавила компонент поисковой строки шестого задания

---
+++ workspace_komissarova_6/js/searchLine.js
@@ -0,0 +1,13 @@
+﻿Vue.component('search-line', {
+   data(){
+       return {
+           searchLine: '',
+       }
+   },
+    template: `<form action="#" class="search-form" @submit.prevent='$parent.$refs.products.filter(searchLine)'>
+                <input type="text" class="search-field" v-model="searchLine">
+                <button class="search-btn" type="submit">Искать<i class="fas fa-search"></i>     
+                </button><br>
+                <p>Вы искали: {{ searchLine }}</p>
+            </form>`          
+});
